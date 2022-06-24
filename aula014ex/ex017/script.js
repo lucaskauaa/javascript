f9e8e2bbcd3daf1e9ea0244497c8947360a7377d
Lucas Kauã <104635659+lucaskauaa@users.noreply.github.com>
2022-06-24 15:01:02 -0300

    fiz exercicos 16 e 17

--- aula014ex/ex017/script.js
+++ aula014ex/ex017/script.js
@@ -1,1 +1,20 @@
-alert ('Olá')
+function tabuada() {
+   let num = document.getElementById('txtn')
+   let tab = document.getElementById('seltab')
+
+   if (num.value.length == 0){
+    window.alert('[ERRO] digite um numero!')
+   } else {
+    let n = Number(num.value)
+    let c = 1
+    tab.innerHTML =''
+    while (c <= 10) {
+        let item = document.createElement('option')
+        item.text = `${n} x ${c} = ${n*c}`
+        tab.appendChild(item)
+        item.value = `tab ${c}`
+        c++
+    }
+   }
+   
+}
